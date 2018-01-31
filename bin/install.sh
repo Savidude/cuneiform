c46d69cd3928f60ea073dd4d87198300867a7e64
Savidude <mysticsavy@gmail.com>
2018-02-01 00:50:43 +0530

    Obtaining punkt from NLTK downloader

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,9 @@
 
 echo "Installing NLTK dependencies"
 pip3 install nltk
+python3 -m nltk.downloader punkt
 
 currentDir=$PWD
 knowledgeDir=$currentDir/../resources/knowledge/db
-touch $knowledgeDir/test.db
+touch $knowledgeDir/system.db
+chmod 777 $knowledgeDir/system.db
